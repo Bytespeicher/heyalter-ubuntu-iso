91c3db1726140338fc81a19586bf72cf5751aa88
Peter Sattler <psattler@hasendamm.de>
2024-02-17 15:50:44 +0000

    Update 010_remove_firstboot_script.sh, added stop unattended-upgrades.service

--- setup/setup_steps/010_remove_firstboot_script.sh
+++ setup/setup_steps/010_remove_firstboot_script.sh
@@ -2,3 +2,8 @@
 
 # FirstBoot Script zum Aufruf der setup.sh wieder löschen
 rm -f /home/schule/.config/autostart/firstboot.desktop
+
+#
+# Stop Update Service
+#
+sudo systemctl stop unattended-upgrades.service
