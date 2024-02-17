c55a70e39de131951703e581d1b34c06c0f08aa3
Peter Sattler <psattler@hasendamm.de>
2024-02-17 15:53:30 +0000

    Update 040_test_audio.sh, vereinfacht

--- setup/setup_steps/040_test_audio.sh
+++ setup/setup_steps/040_test_audio.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Audio testen
-zenity --info --text "OK klicken um Audio zu testen"
 
 arecord|aplay &
 PID=$!
 
-zenity --info --text "Du solltest ein Echo hören. OK klicken um Audio-Test zu beenden"
+zenity --info --text "Audio Test ist gestartet. Du solltest ein Echo hören. OK klicken um Audio-Test zu beenden"
 
 kill $PID
